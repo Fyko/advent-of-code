f4210320c7a01fc24950e247ad203332df23315c
Carter <me@fyko.net>
2022-12-07 14:54:12 -0700

    refactor(07): now that ive regained consciousness

--- 2022/07/src/main.rs
+++ 2022/07/src/main.rs
@@ -1,119 +1,92 @@
-use std::collections::VecDeque;
-
 static INPUT: &str = include_str!("input.txt");
 
 fn main() {
-	println!("Day 07, part 1: {}", part1(INPUT));
-	println!("Day 07, part 2: {}", part2(INPUT));
+	println!("Day 07, part 1: {}", part_one(INPUT));
+	println!("Day 07, part 2: {}", part_two(INPUT));
 }
 
-fn part1(input: &'static str) -> usize {
-    let dirs = parse_dirs(input);
-    dirs.iter().filter(|c| **c <= 100000).sum::<usize>()
+fn part_one(input: &'static str) -> usize {
+	let dir = parse(input);
+
+	dir.sizes().iter()
+		.filter(|size| **size <= 100000)
+		.sum::<usize>()
 }
 
-fn part2(input: &'static str) -> usize {
-    let dirs = parse_dirs(input);
+fn part_two(input: &'static str) -> usize {
+	let dir = parse(input);
 
-    let available = 70000000;
-    let needs = 30000000;
-    let unused = available - dirs.last().unwrap();
-    let target = needs - unused;
+	let available = 70_000_000;
+	let needs = 30_000_000;
+	let unused = available - dir.size();
+	let target = needs - unused;
 
-    *dirs.iter().find(|s| **s >= target).unwrap()
-}
-
-fn parse_dirs(input: &'static str) -> Vec<usize> {
-    let root = Element::from(input);
-
-    let mut dirs = Vec::new();
-    walk(&root, &mut dirs);
-    dirs.sort();
-
-    dirs
-}
-
-fn walk(node: &Element, dirs: &mut Vec<usize>) {
-    dirs.push(node.size());
-
-    for sub in node.subdirs().unwrap() {
-        walk(sub, dirs);
-    }
+	*dir.sizes().iter().find(|s| **s >= target).unwrap()
 }
 
 #[derive(Debug)]
-enum Command {
-    Cd(&'static str),
-    Ls(Vec<&'static str>),
+struct FileSystem {
+	subdirs: Vec<FileSystem>,
+	files: Vec<usize>,
 }
 
-#[derive(Debug)]
-enum Element {
-    Dir(Vec<Element>),
-    File(usize),
-}
+impl FileSystem {
+	fn new() -> FileSystem {
+		FileSystem {
+			subdirs: Vec::new(),
+			files: Vec::new(),
+		}
+	}
 
-impl Element {
     fn size(&self) -> usize {
-        match self {
-            Element::Dir(contents) => contents.iter().map(|e| e.size()).sum::<usize>(),
-            Element::File(s) => *s,
-        }
+        let file_size = self.files.iter().sum::<usize>();
+        let dir_size = self
+            .subdirs
+            .iter()
+            .map(|dir| dir.size())
+            .sum::<usize>();
+
+        file_size + dir_size
     }
 
-    fn is_dir(&self) -> bool {
-        match self {
-            Element::Dir(_) => true,
-            Element::File(_) => false,
-        }
-    }
-
-    fn subdirs(&self) -> Option<Vec<&Element>> {
-        match self {
-            Element::Dir(contents) => Some(contents.iter().filter(|e| e.is_dir()).collect()),
-            Element::File(_) => None,
-        }
+    fn sizes(&self) -> Vec<usize> {
+        self.subdirs
+            .iter()
+            .flat_map(|dir| dir.sizes())
+            .chain([self.size()])
+            .collect()
     }
 }
 
-impl From<&'static str> for Element {
-    fn from(data: &'static str) -> Self {
-        let mut commands = data
-            .split("$ ")
-            .skip(1)
-            .map(|l| match &l[..=1] {
-                "cd" => Command::Cd(l.split_whitespace().last().unwrap()),
-                "ls" => Command::Ls(l.lines().skip(1).collect()),
-                _ => panic!(),
-            })
-            .collect::<VecDeque<_>>();
+fn parse(data: &'static str) -> FileSystem {
+	let mut state = vec![FileSystem::new()];
 
-        fn handle_cmd(commands: &mut VecDeque<Command>) -> Vec<Element> {
-            let mut cwd = Vec::new();
+	for line in data.lines() {
+		match line {
+			"$ ls" => (),
+			"$ cd /" => (),
+			"$ cd .." => {
+				let dir = state.pop().unwrap();
+				state.last_mut().unwrap().subdirs.push(dir);
+			}
+			line if line.starts_with("$ cd ") => {
+				state.push(FileSystem::new());
+			}
+			line if line.starts_with("dir ") => (),
+			line => {
+				let file = line
+					.split_once(' ')
+					.map(|(size, _)| size.parse().unwrap())
+					.unwrap();
+				state.last_mut().unwrap().files.push(file);
+			}
+		}
+	}
 
-            while let Some(cmd) = commands.pop_front() {
-                match cmd {
-                    Command::Cd(path) if path == ".." => return cwd,
-                    Command::Cd(_) => cwd.push(Element::Dir(handle_cmd(commands))),
-                    Command::Ls(listing) => {
-                        for file in listing.iter().filter_map(|l| {
-                            let (a, _) = l.split_once(' ').unwrap();
-                            if let Ok(bytes) = a.parse::<usize>() {
-                                Some(Element::File(bytes))
-                            } else {
-                                None
-                            }
-                        }) {
-                            cwd.push(file);
-                        }
-                    }
-                }
-            }
+	while state.len() > 1 {
+		let dir = state.pop().unwrap();
+		state.last_mut().unwrap().subdirs.push(dir);
+	}
 
-            cwd
-        }
-
-        let mut elements = handle_cmd(&mut commands);
-        elements.remove(0)
-    }
+	state.into_iter().next().unwrap()
 }
