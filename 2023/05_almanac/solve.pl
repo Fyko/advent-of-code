d4ed647f10cc284ffcf12b261f382fd4eafadfef
Carter Himmel <me@fyko.net>
2023-12-05 15:19:51 -0700

    feat: day 05

---
+++ 2023/05_almanac/solve.pl
@@ -0,0 +1,109 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Data::Dumper;
+use lib '../../lib';
+use List::Util qw(first min max);
+use AOC::Base qw(main);
+
+sub parse_almanac {
+    my ($input) = @_;
+
+    my @segments = map {
+        s/^\s+|\s+$//g;
+        my $line = (split(/:\s/, $_))[1];
+        [split /\n/, $line];
+    } split /\n\n/, $input;
+
+    my @seeds = split /\s/, shift(@segments)->[0];
+
+    return \@seeds, \@segments;
+}
+
+sub process_input {
+    my (@lines) = @_;
+    return join "\n", @lines;
+}
+
+sub part_one {
+    my (@lines) = @_;
+    my $input = process_input(@lines);
+
+    my ($seeds, $segments) = parse_almanac($input);
+
+    sub determine_range_value {
+        my ($value, @ranges) = @_;
+
+        foreach my $range (@ranges) {
+            my ($destination, $source, $length) = split /\s/, $range;
+            my $outval = $destination + ($value - $source);
+
+            return $outval if $value >= $source && $value < $source + $length;
+        }
+
+        return $value;
+    }
+
+    my @values = map {
+        my $value = $_;
+        foreach my $segment (@$segments) {
+            ($value) = determine_range_value($value, @$segment);
+        }
+
+        $value;
+    } @$seeds;
+
+    return min(@values);
+}
+
+sub part_two {
+    my (@lines) = @_;
+    my $input = process_input(@lines);
+
+    my ($seed_ranges, $segments) = parse_almanac($input);
+
+    sub determine_range_value_skip {
+        my ($value, @ranges) = @_;
+
+        my @sources = ();
+
+        foreach my $range (@ranges) {
+            my ($destination, $source, $length) = split /\s/, $range;
+            push @sources, $source;
+
+            my $outval = $destination + ($value - $source);
+            my $outskip = ($source + $length - 1) - $value;
+
+            return $outval, $outskip if $value >= $source && $value < $source + $length;
+        }
+
+        my @sorted_sources = sort { $a <=> $b } @sources;
+        my $skip = first { $_ > $value } @sorted_sources;
+        $skip = "inf" if !defined $skip;
+
+        return $value, $skip;
+    }
+
+    my $best = "inf";
+    for (my $i = 0 ; $i < @$seed_ranges ; $i += 2) {
+        my ($start, $length) = @{$seed_ranges}[$i, $i + 1];
+        my $skip_window = $length;
+
+        for (my $offset = 0 ; $offset < $length ; $offset += max(1, $skip_window)) {
+            my $value = $start + $offset;
+            $skip_window = $length - $offset;
+
+            foreach my $segment (@$segments) {
+                my ($new_value, $skip) = determine_range_value_skip($value, @$segment);
+                $value = $new_value;
+                $skip_window = min($skip, $skip_window) if $skip < $length - $offset;
+            }
+            $best = min($best, $value);
+        }
+    }
+
+    return $best;
+}
+
+main(\&part_one, \&part_two);
--- lib/AOC/Base.pm
+++ lib/AOC/Base.pm
@@ -2,6 +2,9 @@ package AOC::Base;
 
 use strict;
 use warnings;
+use feature 'say';
+use Term::ANSIColor;
+use Time::HiRes qw(time);
 use Benchmark qw(:all);
 
 require Exporter;
@@ -16,8 +19,9 @@ our %EXPORT_TAGS = (
 
 our @EXPORT_OK = (@{$EXPORT_TAGS{'all'}});
 
+# iterates over the lines of a file, returning a closure
 sub iter_file {
-    open(my $FI, "<", "./input.txt") or die "Unable to open input file: $!.\n";
+    open(my $FI, "<", "./input") or die "Unable to open input file: $!.\n";
     return sub {
         if (my $line = <$FI>) {
             return $line;
@@ -29,33 +33,50 @@ sub iter_file {
     };
 }
 
+# reads the lines of a file into an array
 sub read_lines {
-    open(my $FI, "<", "./input.txt") or die "Unable to open input file: $!.\n";
+    open(my $FI, "<", "./input") or die "Unable to open input file: $!.\n";
     my @lines = map { chomp; $_ } <$FI>;
     close($FI);
 
     return @lines;
 }
 
+# main function for running the advent of code
+# provide `bench` when running cli to benchmark
 sub main {
     my ($alpha, $bravo) = @_;
 
     my @input = read_lines();
+    say "part one: ".colored( ['bold green'], $alpha->(@input));
+    say "part two: ".colored( ['bold green'], $bravo->(@input));
 
-    my $alpha_res = $alpha->(@input);
-    print "part one: $alpha_res\n";
-    my $bravo_res = $bravo->(@input);
-    print "part two: $bravo_res\n";
-
-    # return if argv does not contain word bench
     return if (grep { $_ eq "bench" } @ARGV) == 0;
 
-    my $results = timethese(0, {
+    my $n_seconds = 5; # run for n seconds, modify as needed
+
+    my %benchmarks = (
         'part one' => sub { $alpha->(@input) },
         'part two' => sub { $bravo->(@input) },
-    }, 'none');
+    );
 
-    foreach my $part (keys %$results) {
-        print "$part result:", timestr($results->{$part}), "\n";
+    foreach my $part (keys %benchmarks) {
+        my $start_time = time();
+        my $end_time = $start_time + $n_seconds;
+        my $iterations = 0;
+        my $total_time_ms = 0;
+        my @iteration_times_ms;
+
+        while (time() < $end_time) {
+            my $start = time();
+            $benchmarks{$part}->();
+            my $end = time();
+            my $duration_ms = ($end - $start) * 1_000;
+            $total_time_ms += $duration_ms;
+            $iterations++;
+        }
+
+        my $avg_time_per_iteration_ms = $total_time_ms / $iterations;
+        say "$part: took ".sprintf("%.3f", $total_time_ms)."ms (avg ".(sprintf "%.3f", $avg_time_per_iteration_ms)."ms) (n=$iterations)";
     }
 }
