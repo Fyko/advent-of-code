894bdbe921e11cab3743030d7f95b65963d91be3
Carter Himmel <me@fyko.net>
2023-12-07 21:48:47 -0700

    feat: day 07

--- 2023/07_camel_cards/solve.pl
+++ 2023/07_camel_cards/solve.pl
@@ -18,53 +18,39 @@ use constant {
     RANK_FOUR_OF_A_KIND  => 6,
     RANK_FIVE_OF_A_KIND  => 7, };
 
-sub part_one {
-    my (@lines) = @_;
-    my %cards = map { split /\h/ } @lines;
+sub compute_hand_rank {
+    my ($hand, $check_jokers) = @_;
 
-    # hand => rank
-    my %rankings = ();
-    while (my ($hand, $bid) = each %cards) {
-        my $rank  = 0;
-        my @chars = split //, $hand;
+    my $jokers = $check_jokers ? () = $hand =~ /J/g : 0;
 
-        my %count;
-        $count{ $_ }++ foreach @chars;
-
-        if (grep { $_ == 5 } values %count) {
-            $rank = RANK_FIVE_OF_A_KIND;
-        } elsif (grep { $_ == 4 } values %count) {
-            $rank = RANK_FOUR_OF_A_KIND;
-        } elsif ((grep { $_ == 3 } values %count) && (grep { $_ == 2 } values %count)) {
-            $rank = RANK_FULL_HOUSE;
-        } elsif (grep { $_ == 3 } values %count) {
-            $rank = RANK_THREE_OF_A_KIND;
-        } elsif ((scalar grep { $_ == 2 } values %count) == 2) {
-            $rank = RANK_TWO_PAIR;
-        } elsif ((scalar grep { $_ == 2 } values %count) == 1) {
-            $rank = RANK_PAIR;
-        } elsif (scalar grep { $_ == 1 } values %count == 5) {
-            $rank = RANK_HIGH_CARD;
-        }
-
-        $rankings{ $hand } = $rank;
+    my %count;
+    foreach my $card (split // => $hand) {
+        next if ($card eq 'J' && $check_jokers);
+        $count{ $card }++;
     }
 
-    my %chunks;
-    while (my ($hand, $rank) = each %rankings) {
-        push @{ $chunks{ $rank } }, $hand;
+    my @counts = @count{ sort { $count{$b} <=> $count{$a} } keys %count };
+    my ($x, $y) = @counts;
+
+    return RANK_FIVE_OF_A_KIND if $jokers == 5 || $x + $jokers == 5;
+    return RANK_FOUR_OF_A_KIND if $x + $jokers == 4;
+    return RANK_FULL_HOUSE      if $x + $jokers == 3 && $y == 2;
+    return RANK_THREE_OF_A_KIND if $x + $jokers == 3 && $y != 2;
+    return RANK_TWO_PAIR        if $x + $jokers == 2 && $y == 2;
+    return RANK_PAIR            if $x + $jokers == 2 && $y != 2;
+    return RANK_HIGH_CARD;
+}
+
+sub create_card_groups {
+    my ($rankings, $face_rankings) = @_;
+
+    my %groups;
+    while (my ($hand, $rank) = each %$rankings) {
+        push @{ $groups{ $rank } }, $hand;
     }
 
-    my %card_rankings = (
-        'A' => 14,
-        'K' => 13,
-        'Q' => 12,
-        'J' => 11,
-        'T' => 10,
-    );
-
-    while (my ($rank, $hands) = each %chunks) {
-        @{ $chunks{ $rank } } = sort {
+    while (my ($rank, $hands) = each %groups) {
+        @{ $groups{ $rank } } = sort {
             my @as = split //, $a;
             my @bs = split //, $b;
 
@@ -72,36 +58,94 @@ sub part_one {
             while ($i < scalar @as) {
                 my $as  = $as[$i];
                 my $bs  = $bs[$i];
-                my $cmp = (looks_like_number($as) ? $as : $card_rankings{ $as }) <=> (looks_like_number($bs) ? $bs : $card_rankings{ $bs });
+                my $cmp = (looks_like_number($as) ? $as : $face_rankings->{ $as }) <=> (looks_like_number($bs) ? $bs : $face_rankings->{ $bs });
                 return $cmp if $cmp != 0;
                 ++$i;
             }
 
             return 0;
-        } @{ $chunks{ $rank } };
+        } @{ $groups{ $rank } };
     }
 
-    say Dumper \%chunks;
+    return %groups;
+}
 
-    my @sorted_hands = map { @{ $chunks{ $_ } } } sort { $a <=> $b } keys %chunks;
+sub do_part {
+    my (@lines, $face_rankings, $check_jokers) = @_;
 
-    say Dumper \@sorted_hands;
+    my %cards = map { split /\h/ } @lines;
+
+    my %rankings = ();
+    while (my ($hand, $bid) = each %cards) {
+        my $rank = compute_hand_rank($hand, 0);
+        $rankings{ $hand } = $rank;
+    }
+
+    my %groups = create_card_groups(\%rankings, \%$face_rankings);
+
+    my @sorted_hands = map { @{ $groups{ $_ } } } sort { $a <=> $b } keys %groups;
 
     my $sum = 0;
     while (my ($index, $hand) = each @sorted_hands) {
         my $rank = $index + 1;
         my $bid  = $cards{ $hand };
         my $diff = $rank * $bid;
-        say "$rank: $hand +$diff (\$$bid)";
         $sum += $diff;
     }
 
     return $sum;
 }
 
+sub sum_groups {
+    my ($groups, $cards) = @_;
+
+    my @sorted_hands = map { @{ $groups->{ $_ } } } sort { $a <=> $b } keys %$groups;
+
+    my $sum = 0;
+    while (my ($index, $hand) = each @sorted_hands) {
+        my $rank = $index + 1;
+        my $bid  = %$cards{ $hand };
+        my $diff = $rank * $bid;
+        $sum += $diff;
+    }
+
+    return $sum;
+}
+
+sub part_one {
+    my (@lines) = @_;
+    my %cards = map { split /\h/ } @lines;
+
+    my %rankings = map { my $rank = compute_hand_rank($_, 0); $_ => $rank } keys %cards;
+
+    my %face_rankings = (
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,
+        'J' => 11,
+        'T' => 10,
+    );
+    my %groups = create_card_groups(\%rankings, \%face_rankings);
+
+    return sum_groups(\%groups, \%cards);
+}
+
 sub part_two {
     my (@lines) = @_;
-    return 0;
+    my %cards = map { split /\h/ } @lines;
+
+    my %rankings = map { my $rank = compute_hand_rank($_, 1); $_ => $rank } keys %cards;
+
+    my %card_rankings = (
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,
+        'J' => 1,
+        'T' => 10,
+    );
+    my %groups = create_card_groups(\%rankings, \%card_rankings);
+
+    return sum_groups(\%groups, \%cards);
 }
 
 main("07_camel_cards", \&part_one, \&part_two);
