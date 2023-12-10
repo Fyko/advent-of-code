74eb9059bfa9b09d98d02e8e7de914e23613a533
Carter Himmel <me@fyko.net>
2023-12-09 21:50:52 -0700

    chore: template

---
+++ 2023/xx_tbd/solve.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -d:NYTProf
+
+use strict;
+use warnings;
+use feature 'say';
+use lib './lib';
+
+use Data::Dumper;
+use AOC::Base  qw(main);
+use AOC::Math  qw(lcm);
+
+sub part_one {
+    my (@lines)    = @_;
+
+    my $sum = 0;
+    foreach my $line (@lines) {
+        # todo
+    }
+
+    return $sum;
+}
+
+sub part_two {
+    my (@lines)    = @_;
+
+    my $sum = 0;
+    foreach my $line (@lines) {
+        # todo
+    }
+
+    return $sum;
+}
+
+main("xx_tbd", \&part_one, \&part_two);
